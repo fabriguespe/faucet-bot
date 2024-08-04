0f217dad896237b77125a6dba0f8c7b82fecbe9d
fabri <fguespe@gmail.com>
2024-08-03 21:17:01 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { run, HandlerContext } from "@xmtp/botkit";
+import { run, HandlerContext } from "@xmtp/message-kit";
 import { getRedisClient } from "./lib/redis.js";
 import { LearnWeb3Client, Network } from "./lib/learn-web3.js";
 import { FIVE_MINUTES } from "./lib/constants.js";
@@ -6,44 +6,27 @@ import Mixpanel from "mixpanel";
 
 const mixpanel = Mixpanel.init(process.env.MIX_PANEL as string);
 
-const inMemoryCache = new Map<
-  string,
-  { step: number; lastInteraction: number }
->();
+//Tracks conversation steps
+const inMemoryCacheStep = new Map<string, number>();
 
 run(async (context: HandlerContext) => {
   const { message } = context;
   const redisClient = await getRedisClient();
-  const { content, senderAddress } = message;
+  const {
+    content: { content: text },
+    sender,
+  } = message;
 
   mixpanel.track("Faucet-Visit", {
-    distinct_id: senderAddress,
+    distinct_id: sender.address,
   });
-  const oneHour = 3600000; // Milliseconds in one hour.
-  const now = Date.now(); // Current timestamp.
-  const cacheEntry = inMemoryCache.get(senderAddress); // Retrieve the current cache entry for the sender.
 
-  let reset = false; // Flag to indicate if the interaction step has been reset.
   const defaultStopWords = ["stop", "unsubscribe", "cancel", "list"];
-  if (defaultStopWords.some((word) => content.toLowerCase().includes(word))) {
-    // If its a stop word
-    reset = true;
+  if (defaultStopWords.some((word) => text.toLowerCase().includes(word))) {
+    inMemoryCacheStep.set(sender.address, 0);
   }
-  if (!cacheEntry || now - cacheEntry.lastInteraction > oneHour) {
-    // If there's no cache entry or the last interaction was more than an hour ago, reset the step.
-    reset = true;
-  }
-  if (reset) {
-    inMemoryCache.delete(senderAddress);
-  }
-  // Update the cache entry with either reset step or existing step, and the current timestamp.
-  inMemoryCache.set(senderAddress, {
-    step: reset ? 0 : cacheEntry?.step ?? 0,
-    lastInteraction: now,
-  });
 
-  // Return the updated cache entry and the reset flag.
-  const step = inMemoryCache.get(senderAddress)!.step;
+  const cacheStep = inMemoryCacheStep.get(sender.address) || 0;
 
   const cachedSupportedNetworksData = await redisClient.get(
     "supported-networks"
@@ -81,9 +64,9 @@ run(async (context: HandlerContext) => {
       !n.networkId.toLowerCase().includes("mode")
   );
 
-  if (!step) {
+  if (!cacheStep) {
     // send the first message
-    await context.reply(
+    await context.send(
       "Hey! I can assist you in obtaining testnet tokens.\n\n Type the network number you would like to drip tokens from."
     );
     if (process.env.DEBUG === "true") console.log(supportedNetworks);
@@ -106,62 +89,62 @@ run(async (context: HandlerContext) => {
       .map((n) => `${n.index}. ${n.networkId}`);
 
     //Else display list
-    await context.reply(
+    await context.send(
       `Send "list" at any time to show the list again.\n\n✅With Balance:\n${channelsWithBalance.join(
         "\n"
       )}\n\n❌Without Balance:\n${channelsWithoutBalance.join("\n")}`
     );
 
-    inMemoryCache.set(senderAddress, { step: 1, lastInteraction: Date.now() });
-  } else if (step === 1) {
-    if (content.toLowerCase() === "balances") {
+    inMemoryCacheStep.set(sender.address, 1);
+  } else if (cacheStep === 1) {
+    if (text.toLowerCase() === "balances") {
       //Only for admin purposes
       const networkList = supportedNetworks.map((n) => {
         return `- ${n.networkId}: ${n.balance}`;
       });
 
-      await context.reply(
+      await context.send(
         `Here are the networks you can choose from:\n\n${networkList.join(
           "\n"
         )}\n\nSend "list" at any time to show the list again.`
       );
     }
 
-    const selectedNetworkIndex = parseInt(content) - 1;
+    const selectedNetworkIndex = parseInt(text) - 1;
     const selectedNetwork = supportedNetworks[selectedNetworkIndex];
 
     if (!selectedNetwork) {
-      await context.reply("Invalid option. Please select a valid option.");
+      await context.send("Invalid option. Please select a valid option.");
       return;
     }
-    await context.reply(
+    await context.send(
       "Your testnet tokens are being processed. Please wait a moment for the transaction to process."
     );
     const network = selectedNetwork;
     const result = await learnWeb3Client.dripTokens(
       selectedNetwork.networkId,
-      senderAddress
+      sender.address
     );
 
     mixpanel.track("Faucet-Request", {
-      distinct_id: senderAddress,
+      distinct_id: sender.address,
       network: network.networkName,
     });
     if (!result.ok) {
-      await context.reply(
+      await context.send(
         `❌ Sorry, there was an error processing your request:\n\n"${result.error!}"`
       );
       return;
     }
 
-    await context.reply("Here's your transaction receipt:");
-    await context.reply(
+    await context.send("Here's your transaction receipt:");
+    await context.send(
       `${process.env.FRAME_BASE_URL}?txLink=${result.value}&networkLogo=${
         network?.networkLogo
       }&networkName=${network?.networkName.replaceAll(" ", "-")}&tokenName=${
         network?.tokenName
       }&amount=${network?.dripAmount}`
     );
-    inMemoryCache.set(senderAddress, { step: 0, lastInteraction: Date.now() });
+    inMemoryCacheStep.set(sender.address, 0);
   }
 });
